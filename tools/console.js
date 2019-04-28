8258abcb259b1fd7a8242e47052704a52db80a98
Aditya Agarwal <adityaa803@gmail.com>
2019-04-28 12:48:13 +0530

    simplified REPL with autocomplete

--- tools/console.js
+++ tools/console.js
@@ -5,24 +5,13 @@ const addUser = require('../src/services/addUser');
 const deleteUser = require('../src/services/deleteUser');
 const { requestChangePassword, verifyPassword } = require('../src/services/changeUserPassword');
 
-const context = {
+const s = {
   addUser,
   deleteUser,
   requestChangePassword,
   verifyPassword,
 };
 
-const moduleCompletions = Object.keys(context);
-const commandCompletions = ['.break', '.clear', '.editor', '.exit', '.help', '.load', '.save'];
-const completions = [...moduleCompletions, ...commandCompletions];
+const r = repl.start();
 
-function completer(line) {
-  const hits = completions.filter((c) => c.startsWith(line));
-  // Show all completions if none found
-  return [hits.length ? hits : completions, line];
-}
-
-const r = repl.start({ completer });
-
-Object.assign(r.context, context);
-r.context.completions = completions; // for debugging completions
+r.context.s = s;
